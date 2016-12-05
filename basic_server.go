7904914a620cf954c74b691842aba06b305d3222
maocast <mc3683@columbia.edu>
2016-12-05 14:19:47 -0800

    formating code using gofmt

--- basic_server.go
+++ basic_server.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "net/http"
-    "encoding/json"
-    "io"
+	"encoding/json"
+	"io"
+	"net/http"
 )
 
 //Structures used for encoding/decoding json
 type Person struct {
-    FirstName, LastName string
+	FirstName, LastName string
 }
 
 type Greeter struct {
-    Body string
+	Body string
 }
 
 type ServerError struct {
@@ -20,47 +20,47 @@ type ServerError struct {
 }
 
 func introduce(w http.ResponseWriter, r *http.Request) {
-    //Only allow for post methods
-    if r.Method == "POST" {
-    	p, err := decodeBody(r.Body)
-    	//If there was problem decoding the body parameters
-    	if err != nil {
-    		//Manually writing error message in json format. Should encode data
-	    	http.Error(w, "{\"Message\": \"" + err.Message + "\"}", http.StatusBadRequest)
-	    	return
-    	}
+	//Only allow for post methods
+	if r.Method == "POST" {
+		p, err := decodeBody(r.Body)
+		//If there was problem decoding the body parameters
+		if err != nil {
+			//Manually writing error message in json format. Should encode data
+			http.Error(w, "{\"Message\": \""+err.Message+"\"}", http.StatusBadRequest)
+			return
+		}
 
-    	//There are no problems, generate response
-		g := Greeter {"Hi, " + p.FirstName + " " + p.LastName}
+		//There are no problems, generate response
+		g := Greeter{"Hi, " + p.FirstName + " " + p.LastName}
 		json.NewEncoder(w).Encode(&g)
 
-    } else {
-    	//Manually writing error message in json format. Should encode data
-    	http.Error(w, "{\"Message\": \"Method not allowed\"}", http.StatusMethodNotAllowed)
-    }
+	} else {
+		//Manually writing error message in json format. Should encode data
+		http.Error(w, "{\"Message\": \"Method not allowed\"}", http.StatusMethodNotAllowed)
+	}
 }
 
 func decodeBody(r io.Reader) (*Person, *ServerError) {
-    var p Person
-    decoder := json.NewDecoder(r)
-    err := decoder.Decode(&p)
-    
-    //Problem decoding data
-    if err != nil {
-    	return nil, &ServerError{"Could not load json data"}
-    }
-    //App specific special cases
-    if p.FirstName == "" {
-    	return nil, &ServerError{"Person must have a first name"}
-    }
-    if p.LastName == "" {
-    	return nil, &ServerError{"Person must have a last name"}
-    }
+	var p Person
+	decoder := json.NewDecoder(r)
+	err := decoder.Decode(&p)
 
-    return &p, nil
+	//Problem decoding data
+	if err != nil {
+		return nil, &ServerError{"Could not load json data"}
+	}
+	//App specific special cases
+	if p.FirstName == "" {
+		return nil, &ServerError{"Person must have a first name"}
+	}
+	if p.LastName == "" {
+		return nil, &ServerError{"Person must have a last name"}
+	}
+
+	return &p, nil
 }
 
 func main() {
 	http.HandleFunc("/introduce", introduce)
-    http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
--- basic_server_test.go
+++ basic_server_test.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"net/http"
-	"testing"
 	"bytes"
 	"io/ioutil"
+	"net/http"
 	"strings"
+	"testing"
 )
 
 func TestMainGet(t *testing.T) {
@@ -19,88 +19,88 @@ func TestMainGet(t *testing.T) {
 
 func TestMainPostBadFormat1(t *testing.T) {
 	buf := bytes.NewBufferString("{\"titleasdf")
-    resp, err := http.Post("http://localhost:8080/introduce", "text", buf)
+	resp, err := http.Post("http://localhost:8080/introduce", "text", buf)
 
-    if err != nil {
-    	t.Error("Was not able to comunicate with server")
-    } else if resp.StatusCode != 400 {
-    	t.Error("Server should return 400 error code")
-    }
+	if err != nil {
+		t.Error("Was not able to comunicate with server")
+	} else if resp.StatusCode != 400 {
+		t.Error("Server should return 400 error code")
+	}
 }
 
 func TestMainPostBadFormat2(t *testing.T) {
 	buf := bytes.NewBufferString("{\"firstname\":15}")
-    resp, err := http.Post("http://localhost:8080/introduce", "text", buf)
+	resp, err := http.Post("http://localhost:8080/introduce", "text", buf)
 
-    if err != nil {
-    	t.Error("Was not able to comunicate with server")
-    } else if resp.StatusCode != 400 {
-    	t.Error("Server should return 400 error code")
-    }
+	if err != nil {
+		t.Error("Was not able to comunicate with server")
+	} else if resp.StatusCode != 400 {
+		t.Error("Server should return 400 error code")
+	}
 }
 
 func TestMainPostBadArguments(t *testing.T) {
 	buf := bytes.NewBufferString("{\"title\":\"Incorrect info!.\"}")
-    resp, err := http.Post("http://localhost:8080/introduce", "text", buf)
+	resp, err := http.Post("http://localhost:8080/introduce", "text", buf)
 
-    if err != nil {
-    	t.Error("Was not able to comunicate with server")
-    } else if resp.StatusCode != 400 {
-    	t.Error("Server should return 400 error code")
-    }
+	if err != nil {
+		t.Error("Was not able to comunicate with server")
+	} else if resp.StatusCode != 400 {
+		t.Error("Server should return 400 error code")
+	}
 }
 
 func TestMainPostMissingArguments(t *testing.T) {
 	buf := bytes.NewBufferString("{\"firstname\":\"bob\"}")
-    resp, err := http.Post("http://localhost:8080/introduce", "text", buf)
+	resp, err := http.Post("http://localhost:8080/introduce", "text", buf)
 
-    if err != nil {
-    	t.Error("Was not able to comunicate with server")
-    } else if resp.StatusCode != 400 {
-    	t.Error("Server should return 400 error code")
-    }
+	if err != nil {
+		t.Error("Was not able to comunicate with server")
+	} else if resp.StatusCode != 400 {
+		t.Error("Server should return 400 error code")
+	}
 }
 
 func TestMainPostSuccess1(t *testing.T) {
 	firstname, lastname := "bob", "rogers"
-	buf := bytes.NewBufferString("{\"firstname\":\"" +firstname+ "\", \"lastname\":\"" +lastname+ "\"}")
-    resp, err := http.Post("http://localhost:8080/introduce", "text", buf)
+	buf := bytes.NewBufferString("{\"firstname\":\"" + firstname + "\", \"lastname\":\"" + lastname + "\"}")
+	resp, err := http.Post("http://localhost:8080/introduce", "text", buf)
 
-    if err != nil {
-    	t.Error("Was not able to comunicate with server")
-    } else if resp.StatusCode != 200 {
-    	t.Error("Response should have code 200")
-    } else {
-    	bs, err := ioutil.ReadAll(resp.Body)
-    	if err!= nil {
-    		t.Error("Error reading response")
-    	} else {
-    		response := string(bs)
-    		if !(strings.Contains(response, firstname) && strings.Contains(response , lastname)) {
-    			t.Error("Response does not contain first and last name")
-    		}
-    	}
-    }
+	if err != nil {
+		t.Error("Was not able to comunicate with server")
+	} else if resp.StatusCode != 200 {
+		t.Error("Response should have code 200")
+	} else {
+		bs, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Error("Error reading response")
+		} else {
+			response := string(bs)
+			if !(strings.Contains(response, firstname) && strings.Contains(response, lastname)) {
+				t.Error("Response does not contain first and last name")
+			}
+		}
+	}
 }
 
 func TestMainPostSuccess2(t *testing.T) {
 	firstname, lastname := "jane", "jones"
-	buf := bytes.NewBufferString("{\"firstname\":\"" +firstname+ "\", \"lastname\":\"" +lastname+ "\"}")
-    resp, err := http.Post("http://localhost:8080/introduce", "text", buf)
+	buf := bytes.NewBufferString("{\"firstname\":\"" + firstname + "\", \"lastname\":\"" + lastname + "\"}")
+	resp, err := http.Post("http://localhost:8080/introduce", "text", buf)
 
-    if err != nil {
-    	t.Error("Was not able to comunicate with server")
-    } else if resp.StatusCode != 200 {
-    	t.Error("Response should have code 200")
-    } else {
-    	bs, err := ioutil.ReadAll(resp.Body)
-    	if err!= nil {
-    		t.Error("Error reading response")
-    	} else {
-    		response := string(bs)
-    		if !(strings.Contains(response, firstname) && strings.Contains(response , lastname)) {
-    			t.Error("Response does not contain first and last name")
-    		}
-    	}
-    }
+	if err != nil {
+		t.Error("Was not able to comunicate with server")
+	} else if resp.StatusCode != 200 {
+		t.Error("Response should have code 200")
+	} else {
+		bs, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Error("Error reading response")
+		} else {
+			response := string(bs)
+			if !(strings.Contains(response, firstname) && strings.Contains(response, lastname)) {
+				t.Error("Response does not contain first and last name")
+			}
+		}
+	}
 }
