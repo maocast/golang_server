6bbf003f8df08f1758292bff1e2c9fd86ce6c3ac
maocast <mc3683@columbia.edu>
2016-12-04 19:49:22 -0800

    Adding POST method handling

--- basic_server.go
+++ basic_server.go
@@ -3,10 +3,29 @@ package main
 import (
     "fmt"
     "net/http"
+    "encoding/json"
 )
 
 func introduce(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Path: %s, MethodType: %s", r.URL.Path, r.Method)
+    if r.Method == "POST" {
+    	decoder := json.NewDecoder(r.Body)
+
+		var p Person
+	    err := decoder.Decode(&p)
+	    if err != nil {
+	    	fmt.Println(err)
+	    }
+
+	   	fmt.Println(p.FirstName)
+	    fmt.Println(p.LastName)
+    } else {
+    	fmt.Fprintf(w, "Please use POST method", r.Method)
+    }
+}
+
+type Person struct {
+    FirstName, LastName string
 }
 
 func main() {
